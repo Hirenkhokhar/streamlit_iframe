d6526ce5cc0358d68d18d5aeb2fa8abbac154ed4
Hiren Khokhar <126069290+Hirenkhokhar@users.noreply.github.com>
2025-04-11 11:03:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
-print("hello")
+import streamlit as st
+
+st.set_page_config(page_title="Embedded Streamlit", layout="centered")
+
+st.title("Hello from Streamlit!")
+st.write("This Streamlit app is embedded inside a React website.")
 
